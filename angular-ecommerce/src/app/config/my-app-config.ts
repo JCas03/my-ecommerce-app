fbc516e9f719028fa1c4bfa4e47450468b3273c1
Chris Pierce <cpierce99@satx.rr.com>
2022-03-22 17:17:59 -0500

    .

---
+++ angular-ecommerce/src/app/config/my-app-config.ts
@@ -0,0 +1,10 @@
+export default {
+
+    oidc: {
+        clientId: '0oa4bwu8t6uaTyPQD5d7',
+        issuer: 'https://dev-5165573.okta.com/oauth2/default',
+        redirectUri: 'https://my-ecommerce-app.azurewebsites.net/login/callback',
+        scopes: ['openid', 'profile', 'email']
+    }
+
+}
